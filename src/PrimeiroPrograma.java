1d678b7c0460aac47df1ffdd7adcae7f2bd17892
csvilson <csvilson@gmail.com>
2023-03-03 11:50:15 -0300

    Envio do primeiro código feito no Intellij - DIO

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+import br.com.dio.modelo.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        /*int a = 5;
+        int b = 3;
+
+        System.out.println("resultado da soma: " + (a+b));
+*/
+        Gato gato = new Gato("Luna", "Preta", 5);
+
+        System.out.println(gato);
+    }
+}
